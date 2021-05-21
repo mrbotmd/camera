2d85ad0eb9ddd8c690d00fccb1dc4537838b6293
mrbotmd <mr.bot.md@gmail.com>
2021-05-21 17:50:04 +0600

    dadaw

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/ru-RU";
 
-Vue.config.productionTip = false
+Vue.use(ElementUI, { locale });
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.config.productionTip = false;
+
+const app = new Vue({
+  render: (h) => h(App),
+});
+
+app.$mount("#app");
